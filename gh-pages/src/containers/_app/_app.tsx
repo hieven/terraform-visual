f4e2092ebb514230082ad1e53cd8c15cd05448cd
Even <hieven.c@gmail.com>
2020-08-11 23:35:14 +0200

    Add ga (#11)
    * Add GA
    
    * v1.2.1

--- gh-pages/src/containers/_app/_app.tsx
+++ gh-pages/src/containers/_app/_app.tsx
@@ -1,10 +1,23 @@
 import { Navbar } from '@app/containers/_app'
 import styles from '@app/containers/_app/app.module.css'
 import { TerraofmrPlanProvider } from '@app/context/terraform-plan'
+import { ReactGa } from '@app/utils/google-analytics'
 import Head from 'next/head'
+import { Router } from 'next/router'
+import { useEffect } from 'react'
 
 export const C = ({ Component, pageProps }: any) => {
   const title = 'Terraform Visual'
+
+  useEffect(() => {
+    ReactGa.set({ page: window.location.pathname })
+    ReactGa.pageview(window.location.pathname)
+
+    Router.events.on('routeChangeComplete', () => {
+      ReactGa.set({ page: window.location.pathname })
+      ReactGa.pageview(window.location.pathname)
+    })
+  }, [])
 
   return (
     <>
